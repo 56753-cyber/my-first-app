f20b60624555760edb8855b135d0d4f224cdf34c
56753-cyber <56753@yupparaj.ac.th>
2026-09-16 11:47:07 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -2,14 +2,8 @@ import streamlit as st
 st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
 vat = price * 0.07
-
-
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
-
-
 net_price = price - vat
 t.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
-
-
 st.divider()
 st.write("นางสาว สาริศา สาธรรม เลขที่ 28  ม.4/17")
